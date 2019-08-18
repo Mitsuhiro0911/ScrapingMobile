56ffe85391d4fd32dd1632aceb14c76147325302
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-18 13:03:50 +0900

    ①メインスレッドからHTML通信はできないため、コルーチンで別スレッドを作成 gradle.properties、build.gradle、MainActivity.kt ②デフォルトの設定ではセキュリティの問題でインターネット接続できないため、マニュフェストファイルを修正 AndroidManifest.xml

--- app/src/main/java/com/example/scrapingmobile/MainActivity.kt
+++ app/src/main/java/com/example/scrapingmobile/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.scrapingmobile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // メインスレッドからHTML通信はできないため、コルーチンで別スレッドを作成
+        GlobalScope.launch {
+            val scraping = Scraping(Jsoup.connect("https://ja.wikipedia.org/wiki/メインページ").get())
+        }
     }
 }
--- app/src/main/java/com/example/scrapingmobile/Scraping.kt
+++ app/src/main/java/com/example/scrapingmobile/Scraping.kt
@@ -1,25 +1,28 @@
 package com.example.scrapingmobile
 
-import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
-class Scraping {
-    fun test () {
-        // URLの指定
-        val document = Jsoup.connect("https://ja.wikipedia.org/wiki/メインページ").get()
+class Scraping(val document:Document) {
+    init {
         println(document.html())
+    }
+    private fun getInfoByElement () {
         // 要素の指定
         val elements = document.select("h2")
         for (element in elements) {
             println(element.text())
         }
+    }
+    private fun getInfoByAttr () {
         // 属性の指定
         val attributes = document.select("a").attr("abs:href", "")
         for (attribute in attributes) {
             println(attribute)
         }
+    }
+    private fun getInfoById () {
         // IDの指定
         val id = document.getElementById("top")
         println(id)
     }
-
 }
