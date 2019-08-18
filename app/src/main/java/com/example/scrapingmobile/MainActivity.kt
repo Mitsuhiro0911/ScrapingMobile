f6be6739e1c3325aebf073bef6811c3e3a347c86
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-18 14:12:18 +0900

    抽出条件のチェックボックスの状態に応じて抽出するHTML情報を切り替える処理を実装

--- app/src/main/java/com/example/scrapingmobile/MainActivity.kt
+++ app/src/main/java/com/example/scrapingmobile/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.scrapingmobile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
@@ -11,9 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // メインスレッドからHTML通信はできないため、コルーチンで別スレッドを作成
-        GlobalScope.launch {
-            val scraping = Scraping(Jsoup.connect("https://ja.wikipedia.org/wiki/メインページ").get())
+        val executeButton = findViewById<View>(R.id.executeButton)
+        executeButton.setOnClickListener {
+            // メインスレッドからHTML通信はできないため、コルーチンで別スレッドを作成
+            GlobalScope.launch {
+                val scraping = Scraping(Jsoup.connect("https://ja.wikipedia.org/wiki/メインページ").get())
+                val checkBoxElement = findViewById<View>(R.id.checkBoxElement) as CheckBox
+                if (checkBoxElement.isChecked) {
+                    scraping.getInfoByElement()
+                }
+                val checkBoxAttr = findViewById<View>(R.id.checkBoxAttr) as CheckBox
+                if (checkBoxAttr.isChecked) {
+                    scraping.getInfoByAttr()
+                }
+                val checkBoxId = findViewById<View>(R.id.checkBoxId) as CheckBox
+                if (checkBoxId.isChecked) {
+                    scraping.getInfoById()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/scrapingmobile/Scraping.kt
+++ app/src/main/java/com/example/scrapingmobile/Scraping.kt
@@ -6,21 +6,21 @@ class Scraping(val document:Document) {
     init {
         println(document.html())
     }
-    private fun getInfoByElement () {
+    fun getInfoByElement () {
         // 要素の指定
         val elements = document.select("h2")
         for (element in elements) {
             println(element.text())
         }
     }
-    private fun getInfoByAttr () {
+    fun getInfoByAttr () {
         // 属性の指定
         val attributes = document.select("a").attr("abs:href", "")
         for (attribute in attributes) {
             println(attribute)
         }
     }
-    private fun getInfoById () {
+    fun getInfoById () {
         // IDの指定
         val id = document.getElementById("top")
         println(id)
