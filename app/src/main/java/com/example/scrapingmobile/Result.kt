fe3802bf2b719aa604b76df24697e394a417941a
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-19 20:09:59 +0900

    ResultページのTextViewをスクロールできるように修正

--- app/src/main/java/com/example/scrapingmobile/Result.kt
+++ app/src/main/java/com/example/scrapingmobile/Result.kt
@@ -2,6 +2,7 @@ package com.example.scrapingmobile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -12,6 +13,7 @@ class Result : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
         val textView = findViewById<View>(R.id.textView) as TextView
+        textView.setMovementMethod(ScrollingMovementMethod.getInstance())
         for (element in MainActivity.result) {
             textView.append("${element.text()}\n")
         }
